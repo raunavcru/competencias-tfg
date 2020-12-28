e3a4e6401d8f2a2b2272a7961c97dabdb7c68f0f
raulsfcp4 <36735937+raunavcru@users.noreply.github.com>
2020-12-28 14:06:11 +0100

    [feat] Settings

--- Corrige/Corrige/settings.py
+++ Corrige/Corrige/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'corrige',
         'USER': 'Kelsier',
-        'PASSWORD': 'Kelsier',
+        'PASSWORD': '',
         'HOST': 'localhost',
         'PORT': '5432',
     }
