fd456362b2be27be9247f8cc4673ad791631f161
raulsfcp4 <36735937+raunavcru@users.noreply.github.com>
2021-01-05 13:47:46 +0100

    [feat] Clean

--- Corrige/corrigeapp/forms.py
+++ Corrige/corrigeapp/forms.py
@@ -1,6 +1,9 @@
 from django import forms
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import AuthenticationForm, PasswordChangeForm, UserChangeForm, UserCreationForm
+from django.core.exceptions import ValidationError
+
+from django.utils.timezone import now
 
 User = get_user_model()
 
@@ -26,4 +29,19 @@ class TeacherCreateForm(UserCreationForm):
             'password1',
             'password2'
         )
+
+    def clean_birthdate(self):
+        birthdate = self.cleaned_data.get('birthdate')
+        if birthdate >= now().date():
+            raise ValidationError(
+                'La fecha de cumpleaños debe ser en el pasado')
+        return birthdate
+
+    def clean_username(self):
+        username = self.cleaned_data.get('username')
+        exist = User.objects.filter(username=username)
+        if exist:
+            raise ValidationError('Usuario ya registrado')
+        return username
+    
     
