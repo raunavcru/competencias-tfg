5ad353bdf07cd3bd3b08efd3d0b0fe16e4689b3b
raulsfcp4 <36735937+raunavcru@users.noreply.github.com>
2021-01-15 13:41:50 +0100

    [fix] Settings

--- Corrige/Corrige/settings.py
+++ Corrige/Corrige/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(file)))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
