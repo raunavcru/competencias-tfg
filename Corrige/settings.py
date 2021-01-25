b21b500f6a8002125d1779488c603e64c47cdce5
raulsfcp4 <36735937+raunavcru@users.noreply.github.com>
2021-01-25 20:38:13 +0100

    [release] Setttings

--- Corrige/settings.py
+++ Corrige/settings.py
@@ -112,9 +112,14 @@ LOGGING = {
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default=config('DATABASE_URL')
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'corrige',
+        'USER': 'Kelsier',
+        'PASSWORD': 'Kelsier',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
 }
 
 # Password validation
