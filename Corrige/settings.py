383ad723f8885130a9d6ac9b6af992c5b9faaafe
raulsfcp4 <36735937+raunavcru@users.noreply.github.com>
2021-01-28 14:56:26 +0100

    [release] Env

--- Corrige/settings.py
+++ Corrige/settings.py
@@ -112,7 +112,16 @@ LOGGING = {
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES['default'] =  dj_database_url.config()
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'os.environ["DATABASE_NAME"]',
+        'USER': 'os.environ["DATABASE_USER"]',
+        'PASSWORD': 'os.environ["DATABASE_PASSWORD"]',
+        'HOST': 'os.environ["DATABASE_HOST"]',
+        'PORT': 'os.environ["DATABASE_PORT"]',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
