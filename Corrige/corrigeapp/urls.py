483fe73c3bce474332ac7f8f798296da7f1cbd2f
raulsfcp4 <36735937+raunavcru@users.noreply.github.com>
2021-01-31 15:28:43 +0100

    [feat] Assign

--- Corrige/corrigeapp/urls.py
+++ Corrige/corrigeapp/urls.py
@@ -14,6 +14,7 @@ urlpatterns = [
     path('teachers/list', views.TeachersListView.as_view(), name = 'teachers_list'),
     path('teachers/<int:pk>/update', views.TeacherUpdateView.as_view(), name = 'teachers_update'),
     path('teachers/create', views.TeacherCreateView.as_view(), name = 'teachers_create'),
+    path('sets/<int:pk>/assign', views.SetAssignStudentView.as_view(), name = 'sets_assign_student'),
     path('sets/create', views.SetCreateView.as_view(), name = 'sets_create'),
     path('sets/<int:pk>/update', views.SetUpdateView.as_view(), name = 'sets_update'),
     path('sets/list', views.SetsListView.as_view(), name = 'sets_list'),
@@ -22,6 +23,7 @@ urlpatterns = [
     path('subjects/create', views.SubjectCreateView.as_view(), name = 'subjects_create'),
     path('subjects/<int:pk>/delete', views.SubjectsDeleteView.as_view(), name='subjects_delete'),
     path('subjects/<int:pk>/update', views.SubjectsUpdateView.as_view(), name = 'subjects_update'),
+
 
 
 ]
