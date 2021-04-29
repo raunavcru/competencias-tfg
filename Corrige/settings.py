853497ccb3ebc4781ce77bab8ee6dd7b4d20d6e1
raulsfcp4 <36735937+raunavcru@users.noreply.github.com>
2021-04-29 19:13:09 +0200

    [release] Port

--- Corrige/settings.py
+++ Corrige/settings.py
@@ -114,11 +114,11 @@ LOGGING = {
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'os.environ["DATABASE_NAME"]',
-        'USER': 'os.environ["DATABASE_USER"]',
-        'PASSWORD': 'os.environ["DATABASE_PASSWORD"]',
-        'HOST': 'os.environ["DATABASE_HOST"]',
-        'PORT': 'os.environ["DATABASE_PORT"]',
+        'NAME': os.environ["DATABASE_NAME"],
+        'USER': os.environ["DATABASE_USER"],
+        'PASSWORD': os.environ["DATABASE_PASSWORD"],
+        'HOST': os.environ["DATABASE_HOST"],
+        'PORT': os.environ["DATABASE_PORT"],
     }
 }
 
